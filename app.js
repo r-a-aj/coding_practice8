d1b60c172fd95432c71c6a72d2a86f6cb746d72a
r-a-aj <0redcigar@gmail.com>
2023-12-01 11:40:39 +0000

    Not resolved

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const path = require("path");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const path = require("path");
 
+const dbPath = path.join(__dirname, "todoApplication.db");
 const app = express();
 app.use(express.json());
-const dbPath = path.join(__dirname, "todoApplication.db");
 
 let db = null;
 const initiateDB = async () => {
@@ -15,9 +15,9 @@ const initiateDB = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3000, () => {
-      console.log("Server Running at http://localhost:3000/");
-    });
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
     process.exit(1);
@@ -112,13 +112,13 @@ app.put("/todos/:todoId/", async (request, response) => {
 
   switch (true) {
     case requestBody.status !== undefined:
-      updateColumn = "status";
+      updateColumn = "Status";
       break;
     case requestBody.priority !== undefined:
-      updateColumn = "priority";
+      updateColumn = "Priority";
       break;
     case requestBody.todo !== undefined:
-      updateColumn = "todo";
+      updateColumn = "Todo";
       break;
   }
 
@@ -132,14 +132,14 @@ app.put("/todos/:todoId/", async (request, response) => {
   const {
     todo = previousTodo.todo,
     priority = previousTodo.priority,
-    status = previousTodoQuery.status,
+    status = previousTodo.status,
   } = request.body;
 
   const updateTodoQuery = `
         UPDATE todo
-        SET todo = '${todo}'
-            priority = '${priority}'
-            status = '${status}'
+        SET todo = '${todo}',
+            priority = '${priority}',
+            status = '${status}',
         WHERE id = ${todoId};
     `;
   await db.run(updateTodoQuery);
